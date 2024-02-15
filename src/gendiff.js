b43698d44f81fa2ce5c29c5f745320224b66a03b
Anastasia Zelenkova <nastya.zelenkowa@yandex.ru>
2024-02-15 17:39:33 +0300

    update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
+
 import parse from './parseFile.js';
 import { program } from 'commander';
 import path from 'path';
 import { cwd } from 'process';
 import * as fs from 'node:fs';
+import isEqual  from 'lodash/isEqual.js';
 
 program
     .description('Compares two configuration files and shows a difference.')
@@ -15,9 +18,45 @@ program
         const secondFilePath = path.resolve(`${cwd()}/__fixtures__`, filepath2);
         const file1 = parse(fs.readFileSync(firstFilePath));
         const file2 = parse(fs.readFileSync(secondFilePath));
-        if (typeof file1 === 'object'  && typeof file2 === 'object' && file1 !== null && file2 !== null) {
-            console.log('succses!');
-        }
+        const file1Entries = Object.entries(file1);
+        const file2Entries = Object.entries(file2);
+        const result = [];
+        file1Entries.forEach(([key, value]) => {
+            if (!file2.hasOwnProperty(key)) {
+                result.push({ sign: '-', key, value });
+            } else if (file2.hasOwnProperty(key) && isEqual(value, file2[key] )) {
+                result.push({ sign: ' ', key, value });
+            } else if (file2.hasOwnProperty(key)) {
+                result.push({ sign: '-', key, value });
+                result.push({ sign: '+', key, value: file2[key] });
+            } else {
+                result.push({ sign: '-', key, value });
+            }
+        });
+        file2Entries.forEach(([key, value]) => {
+            if (!file1.hasOwnProperty(key)) {
+                result.push({ sign: '+', key, value });
+            }
+        });
+        result.sort((a, b) => {
+            if (a.key > b.key) {
+               return 1; 
+            }
+            if (a.key === b.key) {
+                if (a.sign === '+') {
+                    return 1;
+                }
+                if (a.sign === '-') {
+                    return -1;
+                }
+            }
+            if (a.key < b.key) {
+                return -1;
+            }
+            return 0;
+        });
+        const resultStr = result.map((entity) => `  ${entity.sign} ${entity.key}: ${entity.value}`);
+        console.log(`{\n${resultStr.join('\n')}\n}`);
     });
 
 program.parse();
